519885f361fe81709ac08b0e0cc12d42d2511a0e
rprit666-source <rprit666@gmail.com>
2025-09-01 11:30:23 +0530

    Delete project directory

--- project/admin/index.php
+++
@@ -1,4 +0,0 @@
-<?php include 'header.php'; ?>
-<h1>Welcome, Admin!</h1>
-<p>This is the admin dashboard. You can manage properties and users from here.</p>
-<?php include 'footer.php'; ?>  
--- project/config.php
+++
@@ -1,3 +0,0 @@
-<?php
-$conn = mysqli_connect("localhost","root","","farm_data") or die("connection failed : " . mysqli_connect_error());
-?>
